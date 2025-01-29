9e3e64dad38a2d6def93742cfd123f8639284347
seongbae15 <pseongbae@gmail.com>
2025-01-29 17:25:06 +0900

    Layout component

--- app/cf/clean.tsx
+++ app/cf/clean.tsx
@@ -1,3 +1,12 @@
 export default function AboutUs() {
     return <div>Clean 1RM</div>
 }
+
+// export const links = () => [
+//     {rel: "stylesheet", href:"cf.com"}
+// ];
+
+// export const meta = () => [
+//     {title: "CrossfitDev"},
+//     {name: "desription", content: "So Hard WOD"}
+// ];
