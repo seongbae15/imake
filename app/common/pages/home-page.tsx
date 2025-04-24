0bb94df1620415124d5ec95bd3aecd3026a307d1
seongbae <pseongbae@gmail.com>
2025-04-24 12:02:00 +0900

    Fix: Fix in routes.ts

---
+++ app/common/pages/home-page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1>Hello</h1>
+      <Button>Get Started</Button>
+    </div>
+  );
+}
