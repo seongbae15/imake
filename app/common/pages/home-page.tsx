1b1321456186b68da4261370b43ae98db453b8dd
seongbae <pseongbae@gmail.com>
2025-05-03 17:57:51 +0900

    Feat: Product cards

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,10 +1,41 @@
+import type { MetaFunction } from "react-router";
+import { ProductCard } from "../../features/product/components/product-card";
 import { Button } from "../components/ui/button";
+import { Link } from "react-router";
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Home | iMake" },
+    { name: "description", content: "Welcome to iMake" },
+  ];
+};
 
 export default function HomePage() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1>Hello</h1>
-      <Button>Get Started</Button>
+    <div className="px-20">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Today's Products
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The best products made by our community today.
+          </p>
+          <Button variant={"link"} asChild className="text-lg p-0">
+            <Link to="/products/leaderboards">Explore all products &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id="productId"
+            name="Product Name"
+            description="Product Description"
+            commentCount={12}
+            viewCount={12}
+            upvoteCount={120}
+          />
+        ))}
+      </div>
     </div>
   );
 }
