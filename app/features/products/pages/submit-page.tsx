d3bb0e9b536b8dc2def942110f945690236aeb5d
seongbae <pseongbae@gmail.com>
2025-05-12 16:16:27 +0900

    Feat Submit product page

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -1,33 +1,115 @@
-import type { MetaFunction } from "react-router";
-import type { Route } from "./+types/app/features/products/pages/submit-page";
-import type { Router } from "@react-router/dev/routes";
+import { Hero } from "~/common/components/hero";
+import type { Route } from "./+types/submit-page";
+import { Form } from "react-router";
+import InputPair from "~/common/components/input-pair";
+import SelectPair from "~/common/components/select-pair";
+import { Label } from "~/common/components/ui/label";
+import { Input } from "~/common/components/ui/input";
+import { useState } from "react";
 
-export function loader({ request }: Route.LoaderArgs) {
-  return {};
-}
-
-export function action({ request }: Route.ActionArgs) {
-  // Handle submission logic here
-  console.log("Product submitted");
-  return { success: true };
-}
-
-export const meta: MetaFunction<Route.MetaArgs> = ({ data }) => {
+export const meta: Route.MetaFunction = () => {
   return [
-    { title: "Submit Product Page" },
+    { title: "Submit Product | iMake" },
     { name: "description", content: "Submit a new product." },
   ];
 };
 
-export default function SubmitPage({
-  loaderData,
-  actionData,
-}: Router.ComponentProps<Route.Return>) {
+export default function SubmitPage({ actionData }: Route.ComponentProps) {
+  const [icon, setIcon] = useState<string | null>(null);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      setIcon(URL.createObjectURL(file));
+    }
+  };
   return (
     <div>
-      <h1>Submit Product Page</h1>
-      {/* Add submission form here */}
-      {actionData?.success && <p>Product submitted successfully!</p>}
+      <Hero
+        title="Submit Your Product"
+        description="Share your product with the world"
+      />
+      <Form className="grid grid-cols-2 gap-10 max-w-screen-lg mx-auto">
+        <div className="space-y-5">
+          <InputPair
+            label="Name"
+            description="This is the name of your product"
+            id="name"
+            name="name"
+            required
+            type="text"
+            placeholder="Name of your product"
+          />
+          <InputPair
+            label="TagLine"
+            description="60 characters or less"
+            id="tagline"
+            name="tagline"
+            required
+            type="text"
+            placeholder="A concise description of your product"
+          />
+          <InputPair
+            label="URL"
+            description="The URL of your product"
+            id="url"
+            name="url"
+            required
+            type="url"
+            placeholder="https://example.com"
+          />
+          <InputPair
+            textArea
+            label="Description"
+            description="A detailed description of your product"
+            id="description"
+            name="description"
+            required
+            placeholder="A detailed description of your product"
+          />
+          <SelectPair
+            label="Category"
+            description="The category of your product"
+            name="category"
+            required
+            placeholder="Select a category"
+            options={[
+              { label: "AI", value: "ai" },
+              { label: "Design", value: "design" },
+              { label: "Marketing", value: "marketing" },
+              { label: "Development", value: "development" },
+            ]}
+          />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <Label className="flex flex-col gap-1 items-baseline">
+            Icon
+            <small className="text-muted-foreground">
+              This is the icon of your product.
+            </small>
+          </Label>
+          <Input
+            type="file"
+            className="w-1/2"
+            onChange={onChange}
+            required
+            name="icon"
+          />
+          <div className="flex flex-col text-xs">
+            <span className="text-muted-foreground">
+              Recommended size: 128x128px
+            </span>
+            <span className="text-muted-foreground">
+              Allowed formats: PNG, JPEG
+            </span>
+            <span className="text-muted-foreground">Max file size: 1MB</span>
+          </div>
+          <div className="size-40 rounded-xl shadow-xl overflow-hidden">
+            {icon ? (
+              <img src={icon} className="object-cover w-full h-full" />
+            ) : null}
+          </div>
+        </div>
+      </Form>
     </div>
   );
 }
