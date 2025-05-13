8efbb2d2add8f9bc5f3728e80b0496d709712599
seongbae <pseongbae@gmail.com>
2025-05-14 04:25:05 +0900

    Feat: Job Detail page

--- app/features/jobs/pages/jobs-page.tsx
+++ app/features/jobs/pages/jobs-page.tsx
@@ -24,7 +24,7 @@ export default function JobsPage() {
       <Hero title="Jobs" description="Companies looking for makers" />
       <div className="grid grid-cols-6 gap-20 items-start">
         <div className="grid grid-cols-3 col-span-4 gap-5">
-          {Array.from({ length: 11 }).map((_, index) => (
+          {Array.from({ length: 20 }).map((_, index) => (
             <JobCard
               key={`jobId-${index}`}
               id={`jobId-${index}`}
@@ -39,7 +39,7 @@ export default function JobsPage() {
             />
           ))}
         </div>
-        <div className="col-span-2 flex flex-col gap-10">
+        <div className="col-span-2 sticky top-20 flex flex-col gap-10">
           <div className="flex flex-col items-start gap-2.5">
             <h4 className="text-sm text-muted-foreground font-bold">Type</h4>
             <div className="flex flex-wrap gap-2">
