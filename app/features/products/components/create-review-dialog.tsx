8974125fa4eeb7ed6af79590af7343fe5a4b2963
seongbae <pseongbae@gmail.com>
2025-05-13 18:32:16 +0900

    Feat: write review

---
+++ app/features/products/components/create-review-dialog.tsx
@@ -0,0 +1,77 @@
+import { Form } from "react-router";
+import InputPair from "~/common/components/input-pair";
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "~/common/components/ui/dialog";
+import { Button } from "~/common/components/ui/button";
+import { Label } from "~/common/components/ui/label";
+import { StarIcon } from "lucide-react";
+import { useState } from "react";
+
+export default function CreateReviewDialog() {
+  const [rating, setRating] = useState<number>(0);
+  const [hoverStar, setHoverStar] = useState<number>(0);
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle className="text-2xl">
+          What do you think of this product?
+        </DialogTitle>
+        <DialogDescription>
+          Share your thoughts and experiences with this products.
+        </DialogDescription>
+      </DialogHeader>
+      <Form className="space-y-6">
+        <div>
+          <Label className="flex-col gap-1 items-start">
+            Rating
+            <small className="text-muted-foreground">
+              What would you rate this product?
+            </small>
+          </Label>
+          <div className="flex gap-2 mt-5">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <label
+                key={star}
+                className="relative"
+                onMouseEnter={() => setHoverStar(star)}
+                onMouseLeave={() => setHoverStar(0)}
+              >
+                <StarIcon
+                  className="size-5 text-yellow-400"
+                  fill={
+                    hoverStar >= star || rating >= star
+                      ? "currentColor"
+                      : "none"
+                  }
+                />
+                <input
+                  type="radio"
+                  value={"star"}
+                  name="rating"
+                  required
+                  className="opacity-0 h-px w-px absolute"
+                  onChange={() => setRating(star)}
+                />
+              </label>
+            ))}
+          </div>
+        </div>
+        <InputPair
+          textArea
+          required
+          label="Review"
+          description="Maximum 1000 character"
+          placeholder="Tell us more about your experience with this product."
+        />
+        <DialogFooter>
+          <Button type="submit">Submit Review</Button>
+        </DialogFooter>
+      </Form>
+    </DialogContent>
+  );
+}
--- app/features/products/pages/product-reviews-page.tsx
+++ app/features/products/pages/product-reviews-page.tsx
@@ -1,5 +1,14 @@
 import { Button } from "~/common/components/ui/button";
 import { ReviewCard } from "../components/review-card";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "~/common/components/ui/dialog";
+import CreateReviewDialog from "../components/create-review-dialog";
 
 export function meta() {
   return [
@@ -10,25 +19,31 @@ export function meta() {
 
 export default function ProductReviewsPage() {
   return (
-    <div className="space-y-10 max-w-xl">
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold">10 Reviews</h2>
-        <Button variant={"secondary"}>Wrtie a review</Button>
+    <Dialog>
+      <div className="space-y-10 max-w-xl">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold">10 Reviews</h2>
+          <DialogTrigger asChild>
+            <Button variant={"secondary"}>Wrtie a review</Button>
+          </DialogTrigger>
+        </div>
+        <div className="space-y-20">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <ReviewCard
+              author={{
+                name: "John Doe",
+                avatarUrl: "https://github.com/seongbae15.png",
+                avatarFallback: "N",
+              }}
+              rating={5}
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              createdAt="10 days ago"
+              key={index}
+            />
+          ))}
+        </div>
       </div>
-      <div className="space-y-20">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <ReviewCard
-            author={{
-              name: "John Doe",
-              avatarUrl: "https://github.com/seongbae15.png",
-              avatarFallback: "N",
-            }}
-            rating={5}
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            createdAt="10 days ago"
-          />
-        ))}
-      </div>
-    </div>
+      <CreateReviewDialog />
+    </Dialog>
   );
 }
