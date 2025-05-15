abe02d88b9d65611e072660818eaec7d5e7e9649
seongbae <pseongbae@gmail.com>
2025-05-15 14:50:08 +0900

    Feat: Team page

--- app/features/teams/pages/team-page.tsx
+++ app/features/teams/pages/team-page.tsx
@@ -1,8 +1,111 @@
+import { Hero } from "~/common/components/hero";
 import type { Route } from "./+types/team-page";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import { Car } from "lucide-react";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/common/components/ui/avatar";
+import { Badge } from "~/common/components/ui/badge";
+import { Form } from "react-router";
+import InputPair from "~/common/components/input-pair";
+import { Button } from "~/common/components/ui/button";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Team Details | iMake" }];
 };
-export function TeamPage() {
-  return <div>Team Page</div>;
+export default function TeamPage() {
+  return (
+    <div className="space-y-20">
+      <Hero title="Join Seongbae's team" />
+      <div className="grid grid-cols-6 gap-40 items-start">
+        <div className="col-span-4 grid grid-cols-4 gap-5">
+          {[
+            { title: "Product name", value: "Doggie Social" },
+            { title: "Stage", value: "MVP" },
+            { title: "Team size", value: 3 },
+            { title: "Available equity", value: 50 },
+          ].map((item) => (
+            <Card key={item.title}>
+              <CardHeader>
+                <CardTitle className="text-sm font-medium text-muted-foreground">
+                  {item.title}
+                </CardTitle>
+                <CardContent className="p-0 font-bold text-2xl">
+                  <p>{item.value}</p>
+                </CardContent>
+              </CardHeader>
+            </Card>
+          ))}
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle className="text-sm font-medium text-muted-foreground">
+                Looking for
+              </CardTitle>
+              <CardContent className="p-0 font-bold text-2xl">
+                <ul className="text-lg list-disc list-inside">
+                  {[
+                    "React Developer",
+                    "Flutter Developer",
+                    "Backend Developer",
+                    "Designer",
+                    "Product Manager",
+                  ].map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle className="text-sm font-medium text-muted-foreground">
+                Idea description
+              </CardTitle>
+              <CardContent className="p-0 font-medium text-xl">
+                <p>
+                  Doggie Social is a social media platform for dog lovers. It
+                  allows dogs to connect with each other and share their
+                  experiences.
+                </p>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        </div>
+        <aside className="col-span-2 space-y-5 border rounded-lg p-6 shadow-sm">
+          <div className="flex gap-5">
+            <Avatar className="size-14">
+              <AvatarFallback>N</AvatarFallback>
+              <AvatarImage src="https://github.com/seongbae15.png" />
+            </Avatar>
+            <div className="flex flex-col">
+              <h4 className="text-lg font-medium">Seongbae</h4>
+              <Badge variant={"secondary"}>Entrepreneur</Badge>
+            </div>
+          </div>
+          <Form className="space-y-5">
+            <InputPair
+              label="Introduce yourself"
+              description="Tell us about yourself"
+              name="introduction"
+              type="text"
+              id="introduction"
+              required
+              textArea
+              placeholder="i.e, I'm ML Engineer with 2 years of experience"
+            />
+            <Button type="submit" className="w-full">
+              Get in touch
+            </Button>
+          </Form>
+        </aside>
+      </div>
+    </div>
+  );
 }
