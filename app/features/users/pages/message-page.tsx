b0e060fbe5d2e03af64e710a899ffcdd8f2a6ea2
seongbae <pseongbae@gmail.com>
2025-05-16 02:53:12 +0900

    Feat: Messages Page

--- app/features/users/pages/message-page.tsx
+++ app/features/users/pages/message-page.tsx
@@ -1,3 +1,4 @@
+import { MessageCircleIcon } from "lucide-react";
 import type { Route } from "./+types/message-page";
 
 export const meta: Route.MetaFunction = () => {
@@ -6,9 +7,11 @@ export const meta: Route.MetaFunction = () => {
 
 function MessagePage() {
   return (
-    <div className="container py-8">
-      <h1 className="text-2xl font-semibold mb-6">Message</h1>
-      <div className="grid gap-6">{/* Message content goes here */}</div>
+    <div className="h-full flex flex-col items-center justify-center gap-4">
+      <MessageCircleIcon className="size-12 text-muted-foreground" />
+      <h1 className="text-xl text-muted-freground font-semibold">
+        Click on a message in the sidebar to view it.
+      </h1>
     </div>
   );
 }
