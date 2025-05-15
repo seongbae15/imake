010b37805ad4dfd06987c3c77d5e1528fbc22c9f
seongbae <pseongbae@gmail.com>
2025-05-16 00:46:41 +0900

    Feat: Notification Page

--- app/features/users/pages/notifications-page.tsx
+++ app/features/users/pages/notifications-page.tsx
@@ -1,3 +1,4 @@
+import { NotificationCard } from "../components/notification-card";
 import type { Route } from "./+types/notifications-page";
 
 export const meta: Route.MetaFunction = () => {
@@ -6,9 +7,18 @@ export const meta: Route.MetaFunction = () => {
 
 function NotificationsPage() {
   return (
-    <div className="container py-8">
-      <h1 className="text-2xl font-semibold mb-6">Notifications</h1>
-      <div className="grid gap-6">{/* Notifications content goes here */}</div>
+    <div className="space-y-20">
+      <h1 className="text-4xl font-bold">Notifications</h1>
+      <div className="flex flex-col items-start gap-5">
+        <NotificationCard
+          avatarUrl="https://github.com/seongbae15.png"
+          avatarFallback="SB"
+          userName="Seongbae"
+          message=" followed you."
+          timeStamp="2 days ago"
+          seen={false}
+        />
+      </div>
     </div>
   );
 }
