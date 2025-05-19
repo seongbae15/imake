7521a2d52908d5d87d8828164c9e46b56cf8a41b
seongbae <pseongbae@gmail.com>
2025-05-20 00:28:19 +0900

    Using DrizzleORM

--- app/features/community/pages/community-page.tsx
+++ app/features/community/pages/community-page.tsx
@@ -12,12 +12,19 @@ import { SORT_OPTIONS, PERIOD_OPTIONS } from "../constant";
 import { Input } from "~/common/components/ui/input";
 import { Button } from "~/common/components/ui/button";
 import { PostCard } from "../components/post-card";
+import { getPosts, getTopics } from "../queries";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Community | iMake" }];
 };
 
-export default function CommunityPage() {
+export const loader = async () => {
+  const topics = await getTopics();
+  const posts = await getPosts();
+  return { topics, posts };
+};
+
+export default function CommunityPage({ loaderData }: Route.ComponentProps) {
   const [searchParams, setSearchParams] = useSearchParams();
   const sorting = searchParams.get("sorting") || "newest";
   const period = searchParams.get("period") || "all";
@@ -92,16 +99,16 @@ export default function CommunityPage() {
             </Button>
           </div>
           <div className="space-y-5">
-            {Array.from({ length: 11 }).map((_, index) => (
+            {loaderData.posts.map((post) => (
               <PostCard
-                key={`postId-${index}`}
-                id={`postId-${index}`}
-                title="What is the best productivity tool?"
-                author="Nico"
-                avatarUrl="https://github.com/apple.png"
-                category="Productivity"
-                timeAgo="12 hours ago"
-                expanded
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                author={post.author}
+                avatarUrl={post.authorAvatarUrl}
+                category={post.topic}
+                timeAgo={post.createdAt}
+                voteCount={post.upvotes}
               />
             ))}
           </div>
@@ -111,17 +118,14 @@ export default function CommunityPage() {
             Topics
           </span>
           <div className="flex flex-col gap-2 items-start">
-            {[
-              "AI Tools",
-              "Design Tools",
-              "Dev Tools",
-              "Note Taking App",
-              "Productivity Tools",
-            ].map((category) => (
-              <Button asChild variant="link" className="pl-0" key={category}>
-                <Link key={category} to={`/community?topics=${category}`}>
-                  {category}
-                </Link>
+            {loaderData.topics.map((topic) => (
+              <Button
+                asChild
+                variant={"link"}
+                key={topic.slug}
+                className="pl-0"
+              >
+                <Link to={`/community?topics=${topic.slug}`}>{topic.name}</Link>
               </Button>
             ))}
           </div>
