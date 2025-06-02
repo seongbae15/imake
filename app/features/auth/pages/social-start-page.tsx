823f6f7c4b6d6e8f422fcd638d8601dd34672ebd
seongbae <pseongbae@gmail.com>
2025-06-02 16:22:04 +0900

    Social Login

--- app/features/auth/pages/social-start-page.tsx
+++ app/features/auth/pages/social-start-page.tsx
@@ -1,5 +1,35 @@
-import { Outlet } from "react-router";
+import { makeSSRClient } from "~/supa-client";
+import type { Route } from "./+types/social-start-page";
+import z from "zod";
+import { redirect } from "react-router";
 
-export default function AuthLayout() {
-  return <div></div>;
-}
+const paramsSchema = z.object({
+  provider: z.enum(["github", "kakao"]),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramsSchema.safeParse(params);
+  if (!success) {
+    return redirect("/auth/login");
+  }
+
+  const { provider } = data;
+  const redirectTo = `http://localhost:5173/auth/social/${provider}/complete`;
+
+  const { client, headers } = makeSSRClient(request);
+  const {
+    data: { url },
+    error,
+  } = await client.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo,
+    },
+  });
+  if (url) {
+    return redirect(url, { headers });
+  }
+  if (error) {
+    throw error;
+  }
+};
