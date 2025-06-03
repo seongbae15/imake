414e80ebd07b91bb420eeb5740563a24c0b96b35
seongbae <pseongbae@gmail.com>
2025-06-03 14:20:22 +0900

    OTP login

--- app/features/auth/pages/otp-complete-page.tsx
+++ app/features/auth/pages/otp-complete-page.tsx
@@ -1,12 +1,45 @@
-import { Form } from "react-router";
+import { Form, redirect, useNavigation, useSearchParams } from "react-router";
 import type { Route } from "./+types/otp-complete-page";
 import InputPair from "~/common/components/input-pair";
 import { Button } from "~/common/components/ui/button";
+import { z } from "zod";
+import { makeSSRClient } from "~/supa-client";
+import { LoaderCircle } from "lucide-react";
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Verfiy OTP | iMake" }];
 };
 
-export default function OtpPage() {
+const formSchema = z.object({
+  email: z.string().email(),
+  otp: z.string().min(6).max(6),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { data, success, error } = formSchema.safeParse(
+    Object.fromEntries(formData)
+  );
+  if (!success) {
+    return { fieldErrors: error.flatten().fieldErrors };
+  }
+  const { email, otp } = data;
+  const { client, headers } = makeSSRClient(request);
+  const { error: verifyError } = await client.auth.verifyOtp({
+    email,
+    token: otp,
+    type: "email",
+  });
+  if (verifyError) {
+    return { verifyError: verifyError.message };
+  }
+  return redirect("/", { headers });
+};
+export default function OtpPage({ actionData }: Route.ComponentProps) {
+  const [searchParams] = useSearchParams();
+  const email = searchParams.get("email");
+  const naviation = useNavigation();
+  const isSubmitting =
+    naviation.state === "submitting" || naviation.state === "loading";
   return (
     <div className="flex flex-col relative items-center justify-center h-full">
       <div className="flex flex-col items-center justify-center w-full max-w-md gap-10">
@@ -16,16 +49,22 @@ export default function OtpPage() {
             Enter the OTP code sent to your email address.
           </p>
         </div>
-        <Form className="w-full space-y-4">
+        <Form className="w-full space-y-4" method="post">
           <InputPair
             label="Email"
             description="Enter your email address"
             name="email"
+            defaultValue={email || ""}
             id="email"
             required
             type="email"
             placeholder="i.e wemake@example.com"
           />
+          {actionData && "fieldErrors" in actionData && (
+            <p className="text-red-500 text-sm">
+              {actionData.fieldErrors?.email?.join(", ")}
+            </p>
+          )}
           <InputPair
             label="OTP"
             description="Enter the OTP you received"
@@ -35,8 +74,20 @@ export default function OtpPage() {
             required
             placeholder="i.e 1234"
           />
-          <Button className="w-full" type="submit">
-            Log in
+          {actionData && "fieldErrors" in actionData && (
+            <p className="text-red-500 text-sm">
+              {actionData.fieldErrors?.otp?.join(", ")}
+            </p>
+          )}
+          {actionData && "verifyError" in actionData && (
+            <p className="text-red-500 text-sm">{actionData.verifyError}</p>
+          )}
+          <Button className="w-full" type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              "Verify OTP"
+            )}
           </Button>
         </Form>
       </div>
