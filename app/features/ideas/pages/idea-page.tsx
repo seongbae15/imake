b48777dcc841462d1e4c36ca98c72f7fcd07e7fe
seongbae <pseongbae@gmail.com>
2025-06-04 18:40:32 +0900

    Claimed ideas

--- app/features/ideas/pages/idea-page.tsx
+++ app/features/ideas/pages/idea-page.tsx
@@ -5,6 +5,9 @@ import type { Route } from "./+types/idea-page";
 import { getGptIdea } from "../queries";
 import { DateTime } from "luxon";
 import { makeSSRClient } from "~/supa-client";
+import { getLoggedInUserId } from "~/features/users/queries";
+import { claimIdea } from "../mutations";
+import { Form, redirect } from "react-router";
 export const meta = ({
   data: {
     idea: { gpt_idea_id, idea },
@@ -17,9 +20,23 @@ export const meta = ({
 };
 
 export const loader = async ({ request, params }: Route.LoaderArgs) => {
-  const { client, headers } = makeSSRClient(request);
+  const { client } = makeSSRClient(request);
   const idea = await getGptIdea(client, params.ideaId);
+  if (idea.is_claimed) {
+    throw redirect(`/ideas`);
+  }
   return { idea };
+};
+
+export const action = async ({ request, params }: Route.ActionArgs) => {
+  const { client } = makeSSRClient(request);
+  const userId = await getLoggedInUserId(client);
+  const idea = await getGptIdea(client, params.ideaId);
+  if (idea.is_claimed) {
+    return { ok: false };
+  }
+  await claimIdea(client, { ideaId: params.ideaId, userId });
+  return redirect(`/my/dashboard/ideas`);
 };
 
 export default function IdeaPage({ loaderData }: Route.ComponentProps) {
@@ -43,7 +60,11 @@ export default function IdeaPage({ loaderData }: Route.ComponentProps) {
             <span>{loaderData.idea.likes}</span>
           </Button>
         </div>
-        <Button size={"lg"}>Claim idea now &rarr;</Button>
+        {loaderData.idea.is_claimed ? null : (
+          <Form method="post">
+            <Button size={"lg"}>Claim idea now &rarr;</Button>
+          </Form>
+        )}
       </div>
     </div>
   );
